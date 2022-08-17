c95645762921c1eb729b0e5b1db74556af99d703
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-08-18 02:18:38 +0530

    Added view file using ejs

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,11 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1>User Profile</h1>');
+    return res.render('profile',{
+        title: 'Profile'
+    });
 }
 
 module.exports.posts = function(req,res){
-    return res.end('<h1>User Posts</h1>');
+    return res.render('Posts',{
+        title: 'Post'
+    });
 }
