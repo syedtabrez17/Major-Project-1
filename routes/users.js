0359327261d4384f4a2e7deabb7f083c980f15bc
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-08-17 22:07:25 +0530

    Added post route for user controller

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const userController = require('../controllers/user_controller')
 
 
+
 router.get('/profile',userController.profile);
+router.use('/posts',userController.posts);
 
 module.exports = router;
