2d46717cfa39d59b5b48f2365ed826cf9b83368c
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-08-20 05:06:05 +0530

    Added Layouts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+// Use of Layouts
+app.use(expressLayouts);
 
 // Use express router
 app.use('/',require('./routes/index'));
