deb483a0e84ee9da8fc2b500999c929e82215415
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-08-27 21:16:30 +0530

    User sign up set up and saved in database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
         title: 'User Profile'
@@ -26,10 +27,33 @@ module.exports.signUp = function(req,res){
 
 // get the sign up data
 module.exports.create = function(req,res){
-
+    // console.log(req.body.password,req.body.confirm_password);
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email : req.body.email}, function(err,user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return
+        }
+        if(!user){
+            User.create(req.body, function(err,user){
+                // console.log(user)
+                // console.log(req.body)
+                if(err){
+                    console.log('Error in creating user while signing up');
+                    return
+                }
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
 
 // get the sign in data
 module.exports.createSession = function(req,res){
-    
+
 }
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,12 @@ const userController = require('../controllers/user_controller')
 
 
 router.get('/profile',userController.profile);
-router.use('/posts',userController.posts);
-router.use('/sign-in',userController.signIn);
-router.use('/sign-up',userController.signUp);
+router.get('/posts',userController.posts);
+
+
+router.get('/sign-in',userController.signIn);
+router.get('/sign-up',userController.signUp);
+
+router.post('/create',userController.create);
 
 module.exports = router;
