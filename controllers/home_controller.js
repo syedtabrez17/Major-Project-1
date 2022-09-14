cb9e535acec3d6da12aa0a746c3861d44402bcd8
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-14 22:04:16 +0530

    Check authentication on creating posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,25 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title : "Home"
-    })
-};
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title : "Codeial | Home",
+    //         posts: posts
+
+    //     });
+    // });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log('Error');return}
+        
+        return res.render('home', {
+            title : "Codeial | Home",
+            posts: posts
+
+        });
+    });
+}
 
 // module.exports.profile = function(req,res){
 //     return res.end('<h1>This is profile</h1>');
