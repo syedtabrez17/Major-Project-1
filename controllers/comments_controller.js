1289c33fe89feeca9078587ca7329a7ca0e8a5e4
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-15 21:02:39 +0530

    Deleting a comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -28,3 +28,23 @@ module.exports.create = function(req, res){
 
     });
 }
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err,comment){
+        if(err){
+            console.log('Error in finding the comment');
+            return;
+        }
+        if(comment.user = req.user.id){
+          let postId = comment.post;
+
+            comment.remove();
+
+          Post.findByIdAndUpdate(postId,{$pull:{comment: req.params.id}}, function(err, post){
+            return res.redirect('back');
+          })  
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
