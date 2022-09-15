d281b22afa6e501b660ad859d1fbb14baf332011
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-15 19:09:02 +0530

    Nested Population : Displaying Comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,10 +9,17 @@ module.exports.home = function(req,res){
 
     //     });
     // });
-
-    Post.find({}).populate('user').exec(function(err, posts){
+    // Populate the user for each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comment',
+        populate:{
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){console.log('Error');return}
-        
         return res.render('home', {
             title : "Codeial | Home",
             posts: posts
