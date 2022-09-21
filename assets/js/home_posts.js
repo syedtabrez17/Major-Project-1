631f22d6167a6272e66639aeb4e17bcc7d114809
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-21 16:16:32 +0530

    Debugged Code

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,5 +1,5 @@
-{
-    // Method to submit the form data for new post using AJAX
+{   
+    // method to submit the form data for new post using AJAX
     let createPost = function(){
         let newPostForm = $('#new-post-form');
 
@@ -10,62 +10,102 @@
                 type: 'post',
                 url: '/posts/create',
                 data: newPostForm.serialize(),
-                success : function(data){
+                success: function(data){
                     let newPost = newPostDom(data.data.post);
-                    $("#posts-list-container>ul").prepend(newPost);
+                    $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost));
-                },
-                error: function(error){
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post published!",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
+                }, error: function(error){
                     console.log(error.responseText);
                 }
             });
         });
     }
 
-    // Method to create a post in DOM
+
+    // method to create a post in DOM
     let newPostDom = function(post){
         return $(`<li id="post-${post._id}">
-        <p>
-            <small>
-               <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a> 
-            </small>
-            ${post.content}
-            <br>
-            <small>
-            ${post.user.name}
-            </small>
-        </p>
-        <div class="post-comment">
-                <form action="comments/create" method="post">
-                    <input name="content" type="text" placeholder="Type here to add comment..." required>
-                    <input type="hidden" name="post" value="${post._id}">
-                    <input type="submit" value="Add Comment">
-                </form>
-            <div class="post-comments-list">
-                <ul id="post-comments-${post._id}">
-                </ul>
-            </div>
-        </div>
-    </li>`);
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${post._id}">X</a>
+                        </small>
+                       
+                        ${post.content}
+                        <br>
+                        <small>
+                        ${post.user.name}
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${post._id}" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </li>`)
     }
+
 
     // method to delete a post from DOM
     let deletePost = function(deleteLink){
         $(deleteLink).click(function(e){
             e.preventDefault();
-        });
 
-        $.ajax({
-            type: 'get',
-            url: $(deleteLink).prop('href'),
-            success: function(data){
-                $(`#post-${data.data.post_id}`).remove();
-            },
-            error: function(error){
-                console.log(error.responseText);
-            }
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
         });
     }
 
+
+
+    let convertPostsToAjax = function(){
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
+        });
+    }
+
+
+
+
+
     createPost();
+    convertPostsToAjax();
 }
