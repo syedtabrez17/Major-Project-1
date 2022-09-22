e182768ba86d1500964178c533ca47ad02ac3a5c
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-22 18:17:48 +0530

    Saving File from Params

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -10,13 +10,40 @@ module.exports.profile = function(req,res){
     });
 }
 
-module.exports.update = function(req, res){
-    if (req.user.id == req.params.id){
-        console.log(req.body)
-        User.findByIdAndUpdate(req.params.id, req.body.name, function(err, user){
-            req.flash('success', 'Updated Successfully!');
-            return res.redirect('back');
-        });
+module.exports.update = async function(req, res){
+    // if (req.user.id == req.params.id){
+    //     console.log(req.body)
+    //     User.findByIdAndUpdate(req.params.id, req.body.name, function(err, user){
+    //         req.flash('success', 'Updated Successfully!');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', 'Unauthorized');
+    //     return res.status(401).send('Unauthorized');
+    // }
+    if(req.user.id == req.params.id){
+        
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if(err){
+                    console.log('Multer Error: ', err);
+                }
+                // console.log(req.file);
+                user.name = req.body.name;
+                if(req.file){
+                    // This is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error', err);
+            return res.redirect('back');;
+        }
+
     }else{
         req.flash('error', 'Unauthorized');
         return res.status(401).send('Unauthorized');
--- models/user.js
+++ models/user.js
@@ -26,10 +26,27 @@ const userSchema = new mongoose.Schema({
     gender:{
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps:true
 });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  });
+
+// static function
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 
