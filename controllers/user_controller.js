5830a40af41f0b1b64615f095fba36f78349becc
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-09-22 21:04:56 +0530

    Unlink the avatar if it already exist for remove redundancy

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req,res){
     User.findById(req.params.id, function(err,user){
@@ -32,6 +34,12 @@ module.exports.update = async function(req, res){
                 // console.log(req.file);
                 user.name = req.body.name;
                 if(req.file){
+
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+
                     // This is saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
