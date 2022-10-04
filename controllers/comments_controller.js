d8ce0605fa5ae444e13e962a224535102bf01ef1
Tabrez Khushter <syedtabrezkhushter@gmail.com>
2022-10-05 00:51:34 +0530

    Setup of kue and paraller job for mailer

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const commentEmailWorker = require('../workers/comment_email_worker');
+const queue = require('../config/kue');
 
 module.exports.create = async function(req, res){
 
@@ -16,7 +18,13 @@ module.exports.create = async function(req, res){
             post.comment.push(comment);
             post.save();
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job = queue.create('emails', comment).save(function(err){
+                if(err){
+                    console.log('Error in creating a queue');
+                }
+                console.log('job enqueued', job.id);
+            });
             
             req.flash('success', 'Comment posted successfully!');
             res.redirect('/');
